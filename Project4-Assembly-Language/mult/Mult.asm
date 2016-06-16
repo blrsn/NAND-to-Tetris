bc88bdfea0a2335a5f7b6ca2c092815b01c10d9d
Berin Larson <berin.larson@in.reportbee.com>
2016-06-17 01:12:30 +0530

    Implemented multiplicatio by repeated addition

--- Project4-Assembly-Language/mult/Mult.asm
+++ Project4-Assembly-Language/mult/Mult.asm
@@ -6,4 +6,49 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+
+// Getting value of R1 from RAM[0]
+@1
+D=M
+
+// setting times to R1
+@times
+M=D
+
+// Getting R2 from RAM[1]
+@0
+D=M
+
+// setting number to R0
+@number
+M=D
+
+// setting variable to zero
+@sum
+M=0
+
+(ADD_LOOP)
+@number
+D=M
+
+@sum
+M=M+D
+
+@times
+M=M-1
+
+D=M
+
+@ADD_LOOP
+D;JGT
+
+// moving value from sum variable to RAM[2]
+@sum
+D=M
+
+@2
+M=D
+
+(INFI_LOOP)
+@INFI_LOOP
+0;JMP
